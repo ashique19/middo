3a82485a1f515385a9b2bc55ec5a28dc7048c1c4
Md Ashiqul Islam <ashique19@gmail.com>
2026-06-16 16:51:02 +0000

    Complete frontend initial development

--- routes/web/kitchen.php
+++ routes/web/kitchen.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Kitchen\DashboardController;
 
 // routes/web/kitchen.php
-Route::middleware(['auth', 'role:kitchen'])->group(function() {
-    Route::get('/kitchen/dashboard', [DashboardController::class, 'index'])->name('kitchen.dashboard');
+Route::middleware(['auth', 'role:kitchen'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('kitchen.dashboard');
 });
